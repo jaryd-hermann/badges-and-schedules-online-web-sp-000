5794f129d3dc69aee56c6e8c0354fb30f151704c
Jaryd Hermann <hermannjaryd@gmail.com>
2021-02-09 23:36:36 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
 # Write your code here.
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  names.collect do |name|
+    badge_maker(name)
+end
+
+def assign_rooms(speakers)
+  room = 0
+  speakers.collect do |name|
+    room+=1
+    "Hello, #{name}! You'll be assigned to room #{room}!"
+  end
+end
+
+def printer(names, speakers)
+
+end
